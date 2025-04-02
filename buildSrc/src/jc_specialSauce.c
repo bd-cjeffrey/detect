78f7837f5c0f98a99dcfbfe95df2b76c7ca35afd
Jeff Campbell <cjeffrey@blackduck.com>
2025-04-02 09:48:04 -0700

    special sauce file

---
+++ buildSrc/src/jc_specialSauce.c
@@ -0,0 +1,10 @@
+// special algorithms included here
+#include <stdio.h>
+
+char key[] = "2943785928759824-2452435298234726";
+
+int specialK() {
+
+     printf("no one should see this");
+     return 0;
+}
